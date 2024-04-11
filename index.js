135e47c012657223932e4f6383a30b49aabc59b8
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-04-11 10:23:52 +0530

    new

---
+++ index.js
@@ -0,0 +1,14 @@
+const accId = 54896;
+let accEmail = "vyassarthak183@gmail.com";
+var Accname = "sarthak";
+accity = "Bhopal";
+
+
+console.log([accId , accEmail , Accname]);
+accEmail = "sarthakvyas@gmail.com";
+Accname = "sarthak";
+console.log(accEmail);
+console.log(Accname);
+//accId = 58;    Not allow 
+console.log(accity);   
+
