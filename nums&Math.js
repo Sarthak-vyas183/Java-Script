984bfa35f0555a90a75688947109913e70075e0d
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-04-12 18:39:36 +0530

    understanding Math's function

---
+++ nums&Math.js
@@ -0,0 +1,33 @@
+const balance = 100;
+//console.log(balance.toString().length)
+//toFixed method use for to take max to decimal : 
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 25.36987
+// console.log(otherNumber.toFixed(2))
+
+const otherNumber = 25.1254;
+//console.log(otherNumber.toPrecision(3)) // writedown but not understand : 
+
+//toLocalString method use to make a large number readable:
+// const bal = 1000000000;
+// console.log(bal.toLocaleString()) //this is acording to us
+// console.log(bal.toLocaleString('en-IN'));
+
+
+//************************Math************************
+
+//console.log(Math.abs) // absolute value
+//abs (absolute value ka use sign change kerne me kiya jata hai (only minus ko pluse me) 
+// const value = -59;
+// const absValue = Math.abs(value);
+// console.log(absValue);
+//console.log(Math.round(4.5));   //it is use to get round value : roundfigure
+//console.log(Math.ceil(4.1));    // it gives top value jese ---4.1 hai to 5dega round value me top wali 
+//console.log(Math.floor(4.9));   // it provides floor value ---jese 4.9 hai to usme se bhi floor wali yani 4 round value dega
+
+
+// console.log(Math.max(4,8,9,7));  // it gives max value
+// console.log(Math.min(9,7,2,5,4)) // it gives min value
+ //console.log(Math.round(6*Math.random()))
+console.log(5*Math.random().toFixed(2))
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+toLocalString method use to make a large number readable:
+const bal = 1000000000;
+console.log(bal.toLocaleString()) //this is acording to us
+console.log(bal.toLocaleString('en-IN'));
