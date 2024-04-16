7f8ab90e658e2ba596e089ea9b6e91e8bb9b05cf
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-04-16 12:01:34 +0530

    IIFE

---
+++ Section-02/IIFE.js
@@ -0,0 +1,12 @@
+// immediate invoke function : 
+// iska use kisi bhi function ko immediate call karne ke liye kiya jata hai 
+
+(function chai() {
+    console.log("DB  connected")        // named function
+}) () ;                              /// ()() => first paranthisis vo fuction wrap and second () execution 
+// if you have two iffe function ek sath then don't miss semicolon (;) 
+
+((name)=>{
+    console.log("DB connected")
+    console.log(name)                            // unamed fucntion
+}) ("sarthak")
---
+++ Section-02/java.js
@@ -0,0 +1,1 @@
+// write a js program to find the last digit of a number 
