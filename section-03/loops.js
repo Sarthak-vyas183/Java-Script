ee9438b5cd87e5abfc3200fa086f1940f86e83e5
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-04-17 18:34:49 +0530

    new files

--- section-03/loops.js
+++ section-03/loops.js
@@ -93,3 +93,103 @@ const college = [
 college.forEach( (item)=>{
    // console.log(`Name of college ${item.collegeName} : package : ${item.package} of course ${item.course}`)
 } )
+
+//*******************Map and filter function***************************** */
+const Arr = [9,116,300,0,1,555,9,182,2,]
+
+const ans = Arr.filter( (num)=>{
+     return num < 100
+} )
+//console.log(ans)
+
+
+const newArr = []
+for(let i=0; i<Arr.length; i++) {
+    if(Arr[i] < 100) {
+       newArr.push(Arr[i]) 
+    }
+ }
+//console.log(newArr)
+
+const books = [
+  { title: 'Book One', genre: 'Fantasy', publish: 2020, edition: 1 },
+  { title: 'Book Two', genre: 'Science Fiction', publish: 2018, edition: 2 },
+  { title: 'Book Three', genre: 'Mystery', publish: 2015, edition: 3 },
+  { title: 'Book Four', genre: 'Historical Fiction', publish: 2019, edition: 1 },
+  { title: 'Book Five', genre: 'Thriller', publish: 2021, edition: 2 },
+  { title: 'Book Six', genre: 'Romance', publish: 2017, edition: 4 },
+  { title: 'Book Seven', genre: 'Non-fiction', publish: 2016, edition: 1 },
+  { title: 'Book Eight', genre: 'Horror', publish: 2022, edition: 1 },
+  { title: 'Book Nine', genre: 'Adventure', publish: 2014, edition: 2 },
+  { title: 'Book Ten', genre: 'Dystopian', publish: 2023, edition: 1 },
+];
+ 
+
+
+// filter function applied on object array 
+const result = books.filter( (book)=>{
+       return book.title === "Book One";
+})  
+
+
+
+//map function are applied on object's array 
+const booktitle = books.map( (book)=>{
+    return book.genre;
+} )
+//console.log(booktitle)
+
+//console.log(result)
+
+//*********************Add ten in every number ********************************************/
+
+const age = [1,2,3,4,5,6,7,8,9,10]
+const newage = age.map( (val)=>{
+      // console.log(val +10)
+      return val + 10;
+} )
+//console.log(newage) 
+
+//***********************intersting reduce function************************************** */
+const item = [1,2,3,4,5]
+const total = item.reduce(  function(accumulator , currentvalue) {
+    //console.log(`accumulator : ${accumulator} and currentvlaue : ${currentvalue}`)
+    return accumulator + currentvalue;
+},0)
+//console.log(total)
+
+
+const shopingcart  = [
+    {
+        name : "js course",
+        price : 2999
+    },
+    {
+        name : "python course",
+        price : 999
+    },
+    {
+        name : "mobile dev course",
+        price : 9999
+    },
+    {
+        name : "java DSA course",
+        price : 4999
+    },
+    {
+        name : "data science course",
+        price : 12999
+    },
+    {
+        name : "React Native course",
+        price : 8999
+    },
+]
+
+const totalPrice = shopingcart.reduce( (accumulator , currentvalue) => {
+        //console.log(`accumulator : ${accumulator} and currentvlaue : ${currentvalue.price}`)
+           return accumulator + currentvalue.price;
+} ,0 )
+
+//console.log(totalPrice)
+
