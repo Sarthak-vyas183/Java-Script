b16293b8ae6c9023ad0aacc3689c3aa06a30db8b
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-04-22 22:22:12 +0530

    prototype

---
+++ AdvanceTopic/object.js
@@ -0,0 +1,22 @@
+multiplyBy5 = function(num) {
+    return num*5;
+}
+
+multiplyBy5.lalu = 2;
+
+console.log(multiplyBy5(5))
+console.log(multiplyBy5.lalu)
+console.log(multiplyBy5.prototype)  
+
+// java script me sabkuchh object hota hai .......ek function bhi hai to vo bhi apne app me 
+// ek object hai : 
+
+function createuser(username , score) {
+    this.username = username
+    this.score  = score
+         
+}
+
+    
+
+
